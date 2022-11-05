42b9cb8bde970ff2d2768610d62dca7c9fab831e
Lacey Weaver <jlweave89@gmail.com>
2022-11-05 08:52:21 -0600

    Add enigma file skeleton

---
+++ lib/enigma.rb
@@ -0,0 +1,17 @@
+class Enigma
+
+  def initialize
+
+  end
+
+  def encrypt(message, key, offset)
+    shift = Shift.new(key, offset)
+    # return_hash = {encryption: ,
+    #     key: "02715",
+    #     offset: "040895"}
+  end
+
+  def decrypt()
+
+  end
+end
