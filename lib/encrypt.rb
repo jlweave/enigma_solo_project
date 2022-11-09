97182392bf11cd608e66d662a83e2a6c1f18860c
Lacey Weaver <jlweave89@gmail.com>
2022-11-08 19:14:30 -0700

    Add fix to encrypt spec file and add test to enigma spec

---
+++ lib/encrypt.rb
@@ -0,0 +1,19 @@
+class Encrypt
+
+
+end
+
+
+
+#message = string.chars.map {|char| char.shift_final.shift_final_key}
+
+
+# #def encrypt(message)
+#   encrypted = " " holds encrypted message at the end
+#   message.split("").to_a.each do |word|
+#     encrypted += shift_final.alaphabet[ (alaphabet.index(word) + key)]
+#   end
+#   return encrypted
+# end
+
+#maybe .collect instead of .each?
