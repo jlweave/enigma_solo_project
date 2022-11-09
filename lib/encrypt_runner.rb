4b64232fbcc2bb3cef61beffed151d677f6b75d4
Lacey Weaver <jlweave89@gmail.com>
2022-11-09 14:18:26 -0700

    Add outline from video

--- lib/encrypt_runner.rb
+++ lib/encrypt_runner.rb
@@ -1,9 +1,11 @@
 require './lib/enigma'
 
 #reading the file 
-
+handle = File.open(ARGV[0], "r")
 #message to be encrypted
-
+incoming_text = handle.read
+handle.close
 #return e_message
-
+writer = File.open(ARGV[1], "w")
 #write to new file and output to terminal
+writer.write()
