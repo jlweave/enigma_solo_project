84c58782d0ee6f07f8a7b2636588d6a3e3951b71
Lacey Weaver <jlweave89@gmail.com>
2022-11-10 08:01:01 -0700

    Add fix for offset conditional

--- lib/offset.rb
+++ lib/offset.rb
@@ -5,8 +5,8 @@ class Offset
     DateTime.now.strftime("%d%m%y")
   end
 
-  def self.square
-    square_num = (current_time.to_i)**2
+  def self.square(conditional = current_time.to_i)
+    square_num = (conditional).to_i**2
     square_num.to_s[-4..-1]
   end
 end
