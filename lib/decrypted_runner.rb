ea01aff4d18caf5beea18e37845844fb8a5400ea
Lacey Weaver <jlweave89@gmail.com>
2022-11-12 16:42:11 -0700

    Add fix to decrypt runner file

--- lib/decrypted_runner.rb
+++ lib/decrypted_runner.rb
@@ -3,9 +3,11 @@ require_relative 'enigma'
 
 read_file = ARGV[0].dup.prepend('./lib/')
 write_file = ARGV[1].dup.prepend('./lib/')
+key = ARGV[2]
+date = ARGV[3]
 
 enigma = Enigma.new
 
-decrypted = enigma.encrypt(File.read(read_file))
+decrypted = enigma.decrypt(File.read(read_file), key, date)
 File.write(write_file, decrypted[:decryption])
-# puts "Created #{ARGV[1]} with the key #{encrypted[:key]} and date #{encrypted[:date]}"
+puts "Created #{ARGV[1]} with the key #{decrypted[:key]} and date #{decrypted[:offset]}"
